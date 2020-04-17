23f1295e6770842309a6e038069093774c7612c9
Ted Blount <52024892+brakluner@users.noreply.github.com>
2020-04-17 03:24:26 -0500

    First Commit
    I'm still working on patching this pre-made front end to a mango db and setting up the routes. So far, I've written out the models folder with the schema and an index js. Next I need to troubleshoot why the next step isnt working and eventually add routes etc.

---
+++ models/Workout.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+      {
+        type: {
+            type: String,
+            trim: true,
+            required: "Type of workout"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Name of workout"
+        },
+        duration: {
+            type: String,
+            trim: true,
+            required: "How Long workout"
+        },
+        weight: {
+            type: Number,
+            trim: true,
+            required: "how many pounds"
+        },
+        reps: {
+            type: Number,
+            trim: true,
+            required: "how many reps?"
+        },
+        sets: {
+            type: Number,
+            trim: true,
+            required: "How Many sets?"
+        },
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
