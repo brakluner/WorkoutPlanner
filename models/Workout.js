9ebea9e082d29be1aa1d88efdd0180c1bbccd065
Ted Blount <52024892+brakluner@users.noreply.github.com>
2020-04-17 20:44:48 -0500

    Inching along
    I've patched it up a little further along. I still need to access what action I need to take next, but I've got the files working together and at least the api routes seem to be working. I think my next step will be tweaking the html routes. and then after that tweaking the stats and exercise pages.

--- models/Workout.js
+++ models/Workout.js
@@ -43,6 +43,6 @@ const workoutSchema = new Schema({
   ]
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const workouts = mongoose.model("Workout", workoutSchema);
 
-module.exports = Workout;
+module.exports = workouts;
